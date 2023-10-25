60eeac978922e6fa839bd2dc6ec070e508eb73cb
Rongxin Liu <rongxinliu.dev@gmail.com>
2023-10-25 14:08:41 -0400

    spawn php server process

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,4 @@
-import { exec } from 'child_process';
+import { spawn } from 'child_process';
 import * as vscode from 'vscode';
 import { PhpLiteAdminProvider } from './phpLiteAdmin';
 
@@ -11,12 +11,24 @@ export async function activate(context: vscode.ExtensionContext) {
     await vscode.commands.executeCommand("setContext", "phpliteadmin50:didActivateExtension", true);
 }
 
-
 function open(fileUri: vscode.Uri) {
 
     // Launch php server
-    exec(`/opt/cs50/bin/phpliteadmin ${fileUri.path}`, {"env": process.env}, () => {
+    console.log("Launching php server");
+    const proc = spawn('/opt/cs50/bin/phpliteadmin', [`${fileUri.path}`], {"env": process.env});
+
+    proc.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+        console.log("Opening phpLiteAdmin window");
         const local_url = `http://127.0.0.1:8082`;
         vscode.env.openExternal(vscode.Uri.parse(local_url));
+    });
+
+    proc.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    proc.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
     });
 }
